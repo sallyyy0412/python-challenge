964a0a693a236b9d0cfb0636d722ac88f88f0eaf
sallyyy0412 <cecilia820412@gmail.com>
2020-12-09 01:50:53 -0800

    homework

---
+++ bybank/main.py
@@ -0,0 +1,67 @@
+import csv
+
+csvpath = "C:/python-challenge/bybank/resources/02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv"
+
+    
+with open(csvpath, newline='') as csvfile:
+	# 讀取 CSV 檔案內容
+	rows = list(csv.reader(csvfile))
+	# 總盈虧
+	total = 0
+	# 總月數 
+	monthCount = 0
+	# 上月盈虧 
+	lastAccount = 0
+	# 總增減 
+	totalChange = 0
+	# 盈利最多月份, 預設為無 
+	greatestIncrease = ['', 0]
+	# 虧損最多月份, 預設為無 
+	greatestDecrease = ['', 0]
+	# 跳過標題row  
+	skipFirstRow = False
+	
+	for row in rows:
+		# 還沒跳過標題row  
+		if not skipFirstRow:
+			# 那就跳過
+			skipFirstRow = True
+			# 下面一位~
+			continue
+		
+		# 本月名稱 
+		currentMonth = row[0]
+		# 本月盈虧
+		currentAccount = int(row[1])
+		# 加總本月盈虧
+		total += currentAccount
+		
+		# 排除第一個月，每個月的增減之加總
+		if lastAccount == 0:
+			# 月增減(與上月比較)
+			monthChange = (currentAccount - lastAccount)
+			# 總增減
+			totalChange += monthChange
+			# 若月增減超過目前最高記錄
+			if monthChange > greatestIncrease[1]:
+				# 則列為新最高記錄
+				greatestIncrease = [currentMonth, monthChange]
+			# 若月增低於過目前最低記錄
+			if monthChange < greatestDecrease[1]:
+				# 則列為新最低記錄
+				greatestDecrease = [currentMonth, monthChange]
+		# 月計數+1
+		monthCount += 1
+		# 列為上個月盈虧
+		lastAccount = currentAccount
+	
+	
+	print('Financial Analysis')
+	print('----------------------------')
+	print('Total Months:', monthCount)
+	print('Total: ', '$' + str(total))
+	print('Average  Change:', '$' + str(round(totalChange / (monthCount - 1), 2)))
+	print('Greatest Increase in Profits:', greatestIncrease[0], '($' + str(greatestIncrease[1]) + ')')
+	print('Greatest Decrease in Profits:', greatestDecrease[0], '($' + str(greatestDecrease[1]) + ')')
+	
+
---
+++ bypoll/main.py
@@ -0,0 +1,53 @@
+import csv
+
+csvpath = "C:/python-challenge/bypoll/resources/02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv"
+
+with open(csvpath, newline='') as csvfile:
+	# 讀取 CSV 檔案內容
+	rows = list(csv.reader(csvfile))
+	# 總投票數
+	totalVotes = 0
+	# 候選人清單
+	candidates = []
+	# 得票清單
+	numberOfVotes = []
+	# 跳過標題row
+	skipFirstRow = False
+	for row in rows:
+		# 還沒跳過標題row
+		if not skipFirstRow:
+			# 那就跳過
+			skipFirstRow = True
+			# 下面一位~
+			continue
+		# 提取候選人名稱
+		candidateName = row[2]
+		# 總投票數+1
+		totalVotes += 1
+		# 若該候選人有在清單內
+		if candidateName in candidates:
+			# 取得索引
+			candidateIndex = candidates.index(candidateName) 
+			# 該索引得票數+1
+			numberOfVotes[candidateIndex] += 1
+		else:
+			# 加入至候選人清單
+			candidates.append(candidateName)
+			# 計1票
+			numberOfVotes.append(1)
+	
+	# 結果輸出
+	print('Election Results')
+	print('-------------------------')
+	print('Total Votes:', totalVotes)
+	print('-------------------------')
+	# 把每位候選人列出
+	for i in range(0, len(candidates)):
+		print(candidates[i] + ':', str('%.3f'%(numberOfVotes[i]/totalVotes * 100)) + '%', '(' + str(numberOfVotes[i]) + ')')
+	print('-------------------------')
+	# 取得最高票數索引
+	maxVoteIndex = numberOfVotes.index(max(numberOfVotes))
+	print('Winner:', candidates[maxVoteIndex])
+	print('-------------------------')
+    
+    
